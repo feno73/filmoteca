44b2602c1c64ef783f415c6f109f538ec743bcf0
feno73 <diego.el.feno@gmail.com>
2020-06-30 08:10:44 -0300

    Menu

---
+++ src/components/menu/Menu.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+export default class Menu extends Component {
+    render() {
+        return (
+            <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+                <a className="navbar-brand" href="#">Filmoteca</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Expandir menú">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarCollapse">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">Lista</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Descargadas</a>
+                        </li>
+                    </ul>
+                    <form className="form-inline mt-2 mt-md-0">
+                        <input className="form-control mr-sm-2" type="text" placeholder="Buscar" aria-label="Buscar"/>
+                        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
+                    </form>
+                </div>
+            </nav>
+        )
+    }
+}
